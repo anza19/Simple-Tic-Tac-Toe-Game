6a72719fef3e0f39ca38a22822c718bd51bda967
anza19 <anza.k.9539@gmail.com>
2019-07-31 10:28:04 -0400

    Commenting code.

---
+++ README.md
@@ -0,0 +1,5 @@
+Steps to compile and run:
+
+1) g++ -c main.cpp -I /usr/include/
+2) g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
+3) ./sfml-app 
--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+//Screen dimensions
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 1024
 
 int main(){
+
+    //This block here renders the window.
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Awesome Game");
 
+    //This is the game while loop
+    //It runs for as long as the window is open
     while(window.isOpen()){
+
+        //Here we create a game object
         sf::Event event;
+
+        //We poll the event here
         while(window.pollEvent(event)){
+
+            //Here we check the event type
             switch(event.type){
+
+                //If its closed, ie event closed
                 case sf::Event::Closed:
+
+                    //We clear and close the window
                     window.close();
                     window.clear();
                     break;
@@ -19,7 +34,9 @@ int main(){
             }
         }
         
+        //Clear the window after every frame
         window.clear();
+
         //objects to draw
         window.display();
 
